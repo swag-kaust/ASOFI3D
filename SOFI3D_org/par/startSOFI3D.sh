65d4bd6d681f40b333667cc6f54d6a7da62ce681
Vladimir Kazei <vkazei@gmail.com>
2016-12-07 11:28:29 +0300

    minor changes

--- SOFI3D_org/par/startSOFI3D.sh
+++ SOFI3D_org/par/startSOFI3D.sh
@@ -4,7 +4,7 @@
 
 #mpirun -np 8 nice -19 ../bin/sofi3D_acoustic ./in_and_out/sofi3D.json | tee ./in_and_out/sofi3D.jout
 #mpirun -np 8 nice -19 ../bin/sofi3D ./in_and_out/sofi3D.inp | tee ./in_and_out/sofi3D.out
-mpirun -np 8 nice -19 ../bin/sofi3D ./in_and_out/sofi3D.json | tee ./in_and_out/sofi3D.jout
+mpirun -np 16 nice -19 ../bin/sofi3D ./in_and_out/sofi3D.json | tee ./in_and_out/sofi3D.jout
 
 #----execute with OPENMPI2
 #mpirun -np 8 nice -19 ../bin/sofi3D ./in_and_out/sofi3D.json | tee ./in_and_out/sofi3D.jout
