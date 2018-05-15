c5223c9ce0228e0251dfd647fb7c7760f542250a
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-05-16 00:04:37 +0300

    [refactoring] Pack velocity derivatives 4 into VelocityDerivativesTensor

--- ASOFI3D/src/zero_elastic.c
+++ ASOFI3D/src/zero_elastic.c
@@ -29,7 +29,7 @@ void zero_elastic(int nx1, int nx2, int ny1, int ny2, int nz1, int nz2, Velocity
                   VelocityDerivativesTensor *dv,
                   VelocityDerivativesTensor *dv_2,
                   VelocityDerivativesTensor *dv_3,
-                  float *** vxyyx_4,float *** vyzzy_4,float *** vxzzx_4,float *** vxxyyzz_4,float *** vyyzz_4,float *** vxxzz_4,float *** vxxyy_4,
+                  VelocityDerivativesTensor *dv_4,
                   float *** svx, float *** svy, float *** svz,
                   float *** svx_2, float *** svy_2, float *** svz_2, float *** svx_3, float *** svy_3, float *** svz_3,
                   float *** svx_4, float *** svy_4, float *** svz_4){
@@ -71,6 +71,14 @@ void zero_elastic(int nx1, int nx2, int ny1, int ny2, int nz1, int nz2, Velocity
     float ***vyyzz_3   = dv_3->yyzz;
     float ***vxxzz_3   = dv_3->xxzz;
     float ***vxxyy_3   = dv_3->xxyy;
+
+    float ***vxyyx_4   = dv_4->xyyx;
+    float ***vyzzy_4   = dv_4->yzzy;
+    float ***vxzzx_4   = dv_4->xzzx;
+    float ***vxxyyzz_4 = dv_4->xxyyzz;
+    float ***vyyzz_4   = dv_4->yyzz;
+    float ***vxxzz_4   = dv_4->xxzz;
+    float ***vxxyy_4   = dv_4->xxyy;
     
     for (j=ny1;j<=ny2;j++){
         for (i=nx1;i<=nx2;i++){
