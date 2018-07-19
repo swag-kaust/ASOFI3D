3c42050e83f07f261830fa68680ca452b7d5f2a7
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-07-19 14:39:16 +0300

    Make test 01 fail if running ASOFI3D fails

--- tests/test_01.sh
+++ tests/test_01.sh
@@ -6,7 +6,7 @@ TEST_PATH="tests/fixtures/test_01"
 mv $MODEL ${MODEL}.bak
 
 # Copy test model.
-cp "${TEST_PATH}/hh_elastic.c"           src/
+cp "${TEST_PATH}/src/hh_elastic.c"       src/
 cp "${TEST_PATH}/in_and_out/sofi3D.json" tmp/in_and_out
 cp "${TEST_PATH}/sources/source.dat"     tmp/sources/
 
@@ -15,12 +15,19 @@ cd src && make sofi3D > /dev/null && cd ..
 
 # Run code.
 ./run_ASOFI3D.sh 16 tmp/ > /dev/null
+code=$?
 
-# Read the files.
-# Compare with the old output.
+if [ "$code" -ne "0" ]; then
+    echo TEST_01: FAIL > /dev/stderr
+    exit 1
+fi
+
+# Convert seismograms in SEG-Y format to the Madagascar RSF format.
 sfsegyread < tmp/su/test_vx.sgy --out=stdout \
     tfile=tmp/su/test_vx_trace.rsf > tmp/su/test_vx.rsf
 
+# Read the files.
+# Compare with the old output.
 tests/compare_datasets.py tmp/su/test_vx.rsf ${TEST_PATH}/su/test_vx.rsf
 result=$?
 if [ "$result" -eq "0" ]; then
