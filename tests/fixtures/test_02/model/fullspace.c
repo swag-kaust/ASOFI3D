c604022f9da8e1cfe96823b8f73ae202050345f6
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-07-25 11:02:33 +0300

    [tests] Modify fitures for test 02

--- tests/fixtures/test_02/model/fullspace.c
+++ tests/fixtures/test_02/model/fullspace.c
@@ -1,248 +1,216 @@
-/* -------------------------------------------------------------
- *   Model for overnight built, fullspace, elastic case
+/*------------------------------------------------------------------------
+ *   Generates elastic model properties (vp,vs,density, Cij coefficients) on the fly
  *
- *   ------------------------------------------------------------- */
+ *   depending on model dimension in vertical direction and local variable "h"
+ *   this function can generate a
+ *   	-> homogeneneous full space
+ *   	-> layer over half space
+ *
+ *  ----------------------------------------------------------------------*/
 
-    #include "fd.h"
+#include "fd.h"
 
-void model_elastic(
-        float ***rho, float ***pi, float ***u, 
-        float ***C11, float ***C12, float ***C13,
-        float ***C22, float ***C23, float ***C33,
-        float ***C44, float ***C55, float ***C66,
-        float ***taus, float ***taup, float *eta)
-{
+void model_elastic(float  ***  rho, float ***  pi, float ***  u,
+        float *** C11, float *** C12, float *** C13, float *** C22, float *** C23, float *** C33,
+        float *** C44, float *** C55, float *** C66,
+        float ***  taus, float ***  taup, float *  eta){
 
-	/*--------------------------------------------------------------------------*/
-	/* extern variables */
-	extern float DT, *FL, TAU;
-	extern int NX, NY, NZ, NXG, NYG, NZG, POS[4], L, MYID;
-	extern char  MFILE[STRING_SIZE];
+    /*--------------------------------------------------------------------------*/
+    /* extern variables */
+    extern float DY;
+    extern int NX, NY, NZ, NXG, NYG, NZG, POS[4], L, MYID;
+    extern char  MFILE[STRING_SIZE];
+    extern int WRITE_MODELFILES;
+    extern FILE *FP;
 
-	/* local variables */
-	float muv, piv, ws;
-	float Vp, Vs, Rho, Qp, Qs;
-	float *pts=NULL, sumu=0.0, sumpi=0.0;
-	float *** pwavemod, *** swavemod;
-	float *** qpmod=NULL, *** qsmod=NULL;
-	
-	int i, j, k, l, ii, jj, kk;
-	int writeallmodels=0;
-	char filename[STRING_SIZE];
-	    
-	    /*-----------------material property definition -------------------------*/
+    /* local variables */
+    float muv, piv;
+    float Vpv, Vsv, Rho, Poi, Epsx, Epsy, Delx, Dely, Delxy, Gamx, Gamy;
+    float eps_1, eps_2, delta_1, delta_2, delta_3, gamma_1, gamma_2;
+    float *** vpv=NULL, *** vsv=NULL, *** epsx=NULL, *** epsy=NULL, *** gamx=NULL;
+    float *** delx=NULL, *** dely=NULL, *** delxy=NULL, *** gamy=NULL;
+    float y;
+    int i, j, k, ii, jj, kk;
+    char modfile[STRING_SIZE];
 
-	    /* parameters for underground */
-	    const float vp1=3500.0, vs1=2000.0, rho1=2000.0,  qp1=20.0, qs1=10.0;
-	    
-	    /*-----------------------------------------------------------------------*/
-	    /*internal switch for writing all models to file (writeallmodels=1)
-	    * or just density (writeallmodels=0)
-	    * BE AWARE that the output of additional models besides density
-	    * cause extra but temporal memory allocation of the size of the
-	    * local subgrid times the number of models!!!!!!!!!! */
-	    writeallmodels=0;
+    /*-----------------material property definition -------------------------*/
 
-	    if (writeallmodels) {
-		    pwavemod  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
-		    swavemod  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
-		    qpmod  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
-		    qsmod  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
-	    }
+    /* x=1, y=2 in Tsvankin [1997] (e.g.) epsx=epsion1 & epsy=epsilon2 */
 
-	    /*elastic simulation */
-	    if (L==0) {
-		    /* loop over global grid */
-		    for (k=1;k<=NZG;k++){
-			    for (i=1;i<=NXG;i++){
-				    for (j=1;j<=NYG;j++){
-					    /*for usability reasons, "z" - as commonly used - denotes the depth (vertical direction),
-					      however, internally "y" is used for the vertical coordinate,
-					      we simply switch the "y" and "z" coordinate as read in the input file,
-					      therefore use the variable "j" to, e.g., calculate/address the vertical coordinate:
-					      x=(float)i*DX;
-					      y=(float)k*DZ;
-					      z=(float)j*DY;*/
-					    /* calculate vertical coordinate in m */
-
-					    /*=========================================================
-					    * modify below to this point for ELASTIC model definition
-					    * visco-elastic model is generated BELOW
-					    *=========================================================
-					    */
-
-					   
-					    Vp=vp1; Vs=vs1; Rho=rho1;
+    /* parameters for layer 1 */
+   /* const float vpv1=2326.0, poi1=0.25,
+     epsx1=0.135, epsy1=-0.082, delx1=-0.166,
+     dely1=-0.24, delxy1=-0.089,
+     gamx1=0.438, gamy1=0.25,
+     rho1=2000.0, h=100000.0; */
+   const float vpv1=3500.0, poi1=0.25,
+           epsx1=0.0, epsy1=0.0,
+           delx1=0.0, dely1=0.0,
+           delxy1=0,
+           gamx1=0.0, gamy1=0.0, rho1=2000.0, h=750000.0;
+    /* parameters for layer 2 */
+    //const float vp2=5700.0, vs2=3400.0, rho2=2500.0;
+    const float vpv2=3000.0, poi2=0.25, epsx2=0., epsy2=0.0, delx2=0., dely2=0., delxy2=0.,
+            gamx2=0., gamy2=0., rho2=1560.0;
 
 
-					    /*=========================================================
-					    * modify up to this point for ELASTIC model definition
-					    * visco-elastic model is generated BELOW
-					    *=========================================================
-					    */
-
-					    muv=Vs*Vs*Rho;
-					    piv=Vp*Vp*Rho;
-
-					    /* only the PE which belongs to the current global gridpoint
-							    is saving model parameters in his local arrays */
-					    if ((POS[1]==((i-1)/NX)) &&
-							    (POS[2]==((j-1)/NY)) &&
-							    (POS[3]==((k-1)/NZ))){
-						    ii=i-POS[1]*NX;
-						    jj=j-POS[2]*NY;
-						    kk=k-POS[3]*NZ;
-
-						    u[jj][ii][kk]=muv;
-						    rho[jj][ii][kk]=Rho;
-						    pi[jj][ii][kk]=piv;
-
-						    if (writeallmodels) {
-							    pwavemod[jj][ii][kk]=Vp;
-							    swavemod[jj][ii][kk]=Vs;
-						    }
-					    }
-				    }
-			    }
-		    }
-	    }
-
-	    if (L){  /*viscoelastic simulation */
-
-		    /* vector for maxwellbodies */
-		    pts=vector(1,L);
-		    for (l=1;l<=L;l++) {
-			    pts[l]=1.0/(2.0*PI*FL[l]);
-			    eta[l]=DT/pts[l];
-		    }
-		    ws=2.0*PI*FL[1];
-
-		    /* loop over global grid */
-		    for (k=1;k<=NZG;k++){
-			    for (i=1;i<=NXG;i++){
-				    for (j=1;j<=NYG;j++){
-
-					    /*for usability reasons, "z" - as commonly used - denotes the depth (vertical direction),
-					      however, internally "y" is used for the vertical coordinate,
-					      we simply switch the "y" and "z" coordinate as read in the input file,
-					      therefore use the variable "j" to, e.g., calculate/address the vertical coordinate:
-					      x=(float)i*DX;
-					      y=(float)k*DZ;
-					      z=(float)j*DY;*/
-					    /* calculate vertical coordinate in m */
-
-					    /*=========================================================
-					    * modify below to this point for VISOCELASTIC model definition
-					    * elastic model is generated above
-					    *=========================================================
-					    */
-
-					    Vp=vp1; Vs=vs1; Rho=rho1; Qp=qp1; Qs=qs1;
-					    
-
-					    /*=========================================================
-					    * modify up to this point for ELASTIC model definition
-					    * elastic model is generated above
-					    *=========================================================
-					    */
-
-					    sumu=0.0;
-					    sumpi=0.0;
-					    for (l=1;l<=L;l++){
-						    sumu=sumu+((ws*ws*pts[l]*pts[l]*(2/Qs))/(1.0+ws*ws*pts[l]*pts[l]));
-						    sumpi=sumpi+((ws*ws*pts[l]*pts[l]*(2/Qp))/(1.0+ws*ws*pts[l]*pts[l]));
-					    }
-
-					    muv=Vs*Vs*Rho/(1.0+sumu);
-					    piv=Vp*Vp*Rho/(1.0+sumpi);
-
-					    /* only the PE which belongs to the current global gridpoint
-							    is saving model parameters in his local arrays */
-					    if ((POS[1]==((i-1)/NX)) &&
-							    (POS[2]==((j-1)/NY)) &&
-							    (POS[3]==((k-1)/NZ))){
-						    ii=i-POS[1]*NX;
-						    jj=j-POS[2]*NY;
-						    kk=k-POS[3]*NZ;
-
-						    u[jj][ii][kk]=muv;
-						    rho[jj][ii][kk]=Rho;
-						    pi[jj][ii][kk]=piv;
-
-						    if (TAU==0.0){
-							    /*calculation of taus and taup by read-in q-files*/
-							    taus[jj][ii][kk]=2/Qs;
-							    taup[jj][ii][kk]=2/Qp;
-						    }
-						    else {
-							    /*constant q (damping) case:*/
-							    taus[jj][ii][kk]=TAU;
-							    taup[jj][ii][kk]=TAU;
-						    }
-
-						    if (writeallmodels) {
-							    pwavemod[jj][ii][kk]=Vp;
-							    swavemod[jj][ii][kk]=Vs;
-							    qsmod[jj][ii][kk]=2/taus[jj][ii][kk];
-							    qpmod[jj][ii][kk]=2/taup[jj][ii][kk];
-						    }
-
-					    }
-				    }
-			    }
-		    }
-	    }
-
-	    /* each PE writes his model to disk */
-
-	    if (writeallmodels) {
-		    sprintf(filename,"%s.SOFI3D.pi",MFILE);
-		    writemod(filename,pi,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-
-		    sprintf(filename,"%s.SOFI3D.u",MFILE);
-		    writemod(filename,u,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-
-		    sprintf(filename,"%s.SOFI3D.vp",MFILE);
-		    writemod(filename,pwavemod,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-
-		    sprintf(filename,"%s.SOFI3D.vs",MFILE);
-		    writemod(filename,swavemod,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-	    }
-
-	    sprintf(filename,"%s.SOFI3D.rho",MFILE);
-	    writemod(filename,rho,3);
-	    MPI_Barrier(MPI_COMM_WORLD);
-	    if (MYID==0) mergemod(filename,3);
-
-	    if ((L) && (writeallmodels)) {
-		    sprintf(filename,"%s.SOFI3D.qp",MFILE);
-		    writemod(filename,qpmod,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-
-		    sprintf(filename,"%s.SOFI3D.qs",MFILE);
-		    writemod(filename,qsmod,3);
-		    MPI_Barrier(MPI_COMM_WORLD);
-		    if (MYID==0) mergemod(filename,3);
-	    }
-
-
-	    free_vector(pts,1,L);
-	    if (writeallmodels) {
-		    free_f3tensor(pwavemod,0,NY+1,0,NX+1,0,NZ+1);
-		    free_f3tensor(swavemod,0,NY+1,0,NX+1,0,NZ+1);
-		    if ((L) && (TAU==0.0)){
-			    free_f3tensor(qpmod,0,NY+1,0,NX+1,0,NZ+1);
-			    free_f3tensor(qsmod,0,NY+1,0,NX+1,0,NZ+1);
-		    }
-	    }
-
+    if (WRITE_MODELFILES==1) {
+        vpv  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        vsv  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        epsx  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        epsy  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        delx  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        dely  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        delxy  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        gamx  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
+        gamy  =  f3tensor(0,NY+1,0,NX+1,0,NZ+1);
     }
+
+    /*elastic simulation */
+    if (L==0) {
+        /* loop over global grid */
+        fprintf(FP,"In HH elastic MYID=%d, POS[1]=%d, POS[2]=%d,POS[3]=%d \n\n",MYID,POS[1],POS[2],POS[3]);
+        for (k=1;k<=NZG;k++){
+            for (i=1;i<=NXG;i++){
+                for (j=1;j<=NYG;j++){
+
+                    /*=========================================================
+                     * modify below this point for ELASTIC model definition
+                     *=========================================================
+                     */
+                    /*note that "y" is used for the vertical coordinate*/
+                    /* calculate vertical coordinate in m */
+
+                    y=(float)j*DY;
+
+
+
+                    /* two layer case */
+                    if (y<=h){
+                        Vpv=vpv1;
+                        Poi=poi1;
+                        Rho=rho1;
+                    } else{
+                        Vpv=vpv2;
+                        Poi=poi2;
+                        Rho=rho2;
+                    }
+                    // perturbation in the midle of the model
+                    if (((i-(NZG/2))*(i-(NZG/2)) + (j-(NZG/2))*(j-(NZG/2)) + (k-(NZG/2))*(k-(NZG/2))) <= 5*5){
+                        //Vpv -= Vpv * 0.1;
+                    }
+
+
+                    /*=========================================================
+                     * modify up to this point for ELASTIC model definition
+                     *=========================================================
+                     */
+
+                    Vsv=Vpv*sqrt((1-2*Poi)/(2-2*Poi));
+                    muv=Vsv*Vsv*Rho;
+                    piv=Vpv*Vpv*Rho;
+
+                    /* only the PE which belongs to the current global gridpoint
+                     * is saving model parameters in his local arrays */
+
+                    if ((POS[1]==((i-1)/NX)) &&
+                            (POS[2]==((j-1)/NY)) &&
+                            (POS[3]==((k-1)/NZ))){
+                        ii=i-POS[1]*NX;
+                        jj=j-POS[2]*NY;
+                        kk=k-POS[3]*NZ;
+
+                        u[jj][ii][kk]=muv;
+                        pi[jj][ii][kk]=piv;
+                        //VTI
+                        C11[jj][ii][kk] = (1+2*Epsx)*Rho*Vpv*Vpv;
+                        C22[jj][ii][kk] = C11[jj][ii][kk];
+                        C33[jj][ii][kk] = Rho*Vpv*Vpv;
+                        C66[jj][ii][kk] = Rho*Vsv*Vsv;
+                        C12[jj][ii][kk] = C11[jj][ii][kk] - 2*C66[jj][ii][kk];
+                        C13[jj][ii][kk]=Rho*sqrt((Vpv*Vpv-Vsv*Vsv)*((1+2*Delx)*Vpv*Vpv-Vsv*Vsv))-Rho*Vsv*Vsv;
+                        C23[jj][ii][kk]=C13[jj][ii][kk];
+                        C44[jj][ii][kk]=Rho*Vsv*Vsv/(1+2*Gamx);
+                        C55[jj][ii][kk]=Rho*Vsv*Vsv/(1+2*Gamx);
+			rho[jj][ii][kk]=Rho;
+
+                        //ORTHO
+                        // C_33 = Rho*Vpv*Vpv;
+                        // C_55 = Rho*Vsv*Vsv;
+                        // C_66 = (1+2*gamma_1)*C_55;
+                        // C_11 = (1+2*eps_2)*C_33;
+                        // C_44 = C_66/(1+2*gamma_2);
+
+                        // C33[jj][ii][kk]=C_33;
+                        // C55[jj][ii][kk]=C_55;
+                        // C11[jj][ii][kk]=C_11;
+                        // C22[jj][ii][kk]=(1+2*eps_1)*C_33;
+                        // C66[jj][ii][kk]=C_66;
+                        // C44[jj][ii][kk]=C_44;
+                        // C13[jj][ii][kk]=-C_55+sqrt(2*C_33*(C_33-C_55)*delta_2 + (C_33-C_55)*(C_33-C_55));
+                        // C12[jj][ii][kk]=-C_66+sqrt(2*delta_3*C_11*(C_11-C_66) + (C_11-C_66)*(C_11-C_66));
+                        // C23[jj][ii][kk]=-C_44+sqrt(2*delta_1*C_33*(C_33-C_44) + (C_33-C_44)*(C_33-C_44));
+                        // rho[jj][ii][kk]=Rho;
+
+                        if (WRITE_MODELFILES==1) {
+                            vpv[jj][ii][kk]=Vpv;
+                            vsv[jj][ii][kk]=Vsv;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    /* each PE writes his model to disk */
+
+    /* all models are written to file */
+    if (WRITE_MODELFILES==1) {
+        sprintf(modfile,"%s.SOFI3D.pi",MFILE);
+        writemod(modfile,pi,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+
+        sprintf(modfile,"%s.SOFI3D.u",MFILE);
+        writemod(modfile,u,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+
+        sprintf(modfile,"%s.SOFI3D.vp",MFILE);
+        writemod(modfile,vpv,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+
+        sprintf(modfile,"%s.SOFI3D.vs",MFILE);
+        writemod(modfile,vsv,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+
+        sprintf(modfile,"%s.SOFI3D.rho",MFILE);
+        writemod(modfile,rho,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+    }
+
+    /* only density is written to file */
+    if (WRITE_MODELFILES==2) {
+        sprintf(modfile,"%s.SOFI3D.rho",MFILE);
+        writemod(modfile,rho,3);
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (MYID==0) mergemod(modfile,3);
+    }
+
+
+    if (WRITE_MODELFILES==1) {
+        free_f3tensor(vpv,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(vsv,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(epsx,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(epsy,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(delx,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(dely,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(delxy,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(gamx,0,NY+1,0,NX+1,0,NZ+1);
+        free_f3tensor(gamy,0,NY+1,0,NX+1,0,NZ+1);
+    }
+}
