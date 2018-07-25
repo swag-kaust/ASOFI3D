f221a6e587eed4687038cb71846e43673667715c
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-07-25 10:58:54 +0300

    [tests] Add abilities to specify tolerances for compared datasets

--- tests/compare_datasets.py
+++ tests/compare_datasets.py
@@ -29,10 +29,18 @@ def main(argv=None):
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument('filename1', help='Filename of the 1st file for comparison')
     p.add_argument('filename2', help='Filename of the 2nd file for comparison')
+    p.add_argument('--rtol', help='Relative tolerance',
+                   type=float, default=1e-5)
+    p.add_argument('--atol', help='Absolute tolerance',
+                   type=float, default=1e-8)
+    p.add_argument('--verbose', '-v', help='Print results to stdout',
+                   action='store_true')
 
     args = p.parse_args()
 
     filename1, filename2 = args.filename1, args.filename2
+    rtol, atol = args.rtol, args.atol
+    verbose = args.verbose
 
     file1 = rsf.Input(filename1)
     file2 = rsf.Input(filename2)
@@ -41,14 +49,20 @@ def main(argv=None):
     data2 = file2.getalldata()
 
     try:
-        result = np.allclose(data1, data2)
+        result = np.allclose(data1, data2, rtol=rtol, atol=atol)
     except ValueError:
+        if verbose:
+            print('Shapes do not match')
         # `np.allclose` returns `ValueError`, when shapes do not match.
         return 2
 
     if result:
+        if verbose:
+            print('Datasets are elementwise close within tolerance')
         return 0
     else:
+        if verbose:
+            print('Datasets are not elementwise close within tolerance')
         return 1
 
 
