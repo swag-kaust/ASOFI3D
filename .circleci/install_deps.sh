0115555ccac745ea45f86b3342ea3aa8def7d569
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-08-02 17:58:14 +0300

    [ci] Add configuration for CircleCI

---
+++ .circleci/install_deps.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Install dependencies necessary for building and running ASOFI3D.
+
+# Install build tools (gcc, openmpi, make, and cmake).
+apt-get -qq update > /dev/null &&
+apt-get --yes --quiet install build-essential cmake > /dev/null
+apt-get --yes --quiet install openmpi > /dev/null
+
+apt-get --yes --quiet install wget > /dev/null
+
+# Install Python 2.7 and Madagascar.
+MINICONDA_URL=https://repo.continuum.io/miniconda
+MINICONDA_FILE=Miniconda2-latest-Linux-x86_64.sh
+
+wget "$MINICONDA_URL/$MINICONDA_FILE" --output-file=wget.log;
+bash "$MINICONDA_FILE" -b -p "$HOME/miniconda"
+export PATH=$HOME/miniconda/bin:$PATH
+
+conda create --yes --name env python=2.7
+source activate env
+conda install --quiet --yes --channel swag-kaust madagascar
+
+# Checking the installed versions
+python -V
+gcc --version
