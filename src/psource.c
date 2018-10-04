cf40a8e60e4234c78861bf1db1578f5bc1b4f8be
vkazei <vkazei@gmail.com>
2018-10-04 11:16:17 +0300

    clean up psource.c now Ricker wavelet is injected like in SAVA for SOURCE_SHAPE=5

--- src/psource.c
+++ src/psource.c
@@ -3,47 +3,40 @@
  *
  *  ----------------------------------------------------------------------*/
 
-#include "fd.h"
 #include "data_structures.h"
+#include "fd.h"
 
-void psource(int nt, Tensor3d *s,
-float **  srcpos_loc, float ** signals, int nsrc, int * stype){
+void psource(int nt, Tensor3d *s, float **srcpos_loc, float **signals, int nsrc, int *stype)
+{
+    extern float DX, DY, DZ;
+    extern float DT;
+    extern int NT;
 
+    int i, j, k, l;
+    float amp = 0.0;
 
-	extern float DX, DY, DZ;
-	//extern float DT;
-	extern int NT;
+    float ***sxx = s->xx;
+    float ***syy = s->yy;
+    float ***szz = s->zz;
 
-	int i, j, k, l;
-	float amp=0.0;
-
-        float ***sxx = s->xx;
-        float ***syy = s->yy;
-        float ***szz = s->zz;
-
-	/* adding source wavelet to stress components 
+    /* adding source wavelet to stress components 
 	   (explosive source) at source points */
 
+    for (l = 1; l <= nsrc; l++)
+    {
+        if (stype[l] == 1)
+        {
+            i = (int)srcpos_loc[1][l];
+            j = (int)srcpos_loc[2][l];
+            k = (int)srcpos_loc[3][l];
 
-	for (l=1;l<=nsrc;l++) {
-	    if(stype[l]==1){
-		i=(int)srcpos_loc[1][l];
-		j=(int)srcpos_loc[2][l];
-		k=(int)srcpos_loc[3][l];
+            // scaled explosive source with respect to spatial
+            // and temporal discretization, seismic Moment = 1 Nm
+            amp = DT * (signals[l][nt]) / (DX * DY * DZ);
 
-		//amp=signals[l][nt]; //unscaled explosive source
-		//amp=(signals[l][nt])/(DX*DY*DZ); //scaled explosive source with respect to spatial discretization, seismic Moment = 1 Nm
-
-		//scaled explosive source with respect to spatial and temporal discretization, seismic Moment = 1 Nm
-		// -> additional temporal derivation
-		if(nt==1){amp=signals[l][nt+1]/(2.0*DX*DY*DZ);}
-		if((nt>1)&&(nt<NT)){amp=(signals[l][nt+1]-signals[l][nt-1])/(2.0*DX*DY*DZ);}
-		if(nt==NT){amp=-signals[l][nt-1]/(2.0*DX*DY*DZ);}
-
-		sxx[j][i][k]+=amp;
-		syy[j][i][k]+=amp;
-		szz[j][i][k]+=amp;
-	    }
-	}
-
+            sxx[j][i][k] -= amp;
+            syy[j][i][k] -= amp;
+            szz[j][i][k] -= amp;
+        }
+    }
 }
--- src/wavelet.c
+++ src/wavelet.c
@@ -7,80 +7,117 @@
 
 #include "fd.h"
 
+float **wavelet(float **srcpos_loc, int nsrc)
+{
+    /* extern variables */
+    extern int SOURCE_SHAPE, NT, MYID;
+    extern float DT;
+    extern char SIGNAL_FILE[STRING_SIZE];
+    extern FILE *FP;
 
-float ** wavelet(float ** srcpos_loc, int nsrc){
+    /*local variables */
+    int nts, nt, k;
+    float *psource = NULL, tshift, amp = 0.0, amp_1 = 0.0, a, fc, tau, t, ts;
+    float **signals;
+    char errormessage[STRING_SIZE];
 
+    if ((SOURCE_SHAPE == 3) && (nsrc > 0))
+    {
+        psource = rd_sour(&nts, fopen(SIGNAL_FILE, "r"));
+        if (nts != NT)
+        {
+            sprintf(errormessage, " Number of samples in external source file (nts = %d) does not equal number of time steps (NT = %d)!", nts, NT);
+            err(errormessage);
+        }
+    }
 
-	/* extern variables */
-	extern int SOURCE_SHAPE, NT, MYID;
-	extern float  DT;
-	extern char SIGNAL_FILE[STRING_SIZE];
-	extern FILE *FP;
+    signals = fmatrix(1, nsrc, 1, NT);
 
-	/*local variables */
-	int nts, nt, k;
-	float *psource=NULL, tshift, amp=0.0, a, fc, tau, t, ts;
-	float ** signals;
-	char errormessage[STRING_SIZE];
+    for (nt = 1; nt <= NT; nt++)
+    {
+        t = (float)nt * DT;
 
+        for (k = 1; k <= nsrc; k++)
+        {
+            tshift = srcpos_loc[4][k];
+            fc = srcpos_loc[5][k];
+            a = srcpos_loc[6][k];
+            ts = 1.0 / fc;
 
-	if ((SOURCE_SHAPE==3) && (nsrc>0)) {
-		psource=rd_sour(&nts,fopen(SIGNAL_FILE,"r"));
-		if (nts != NT) {
-			sprintf(errormessage," Number of samples in external source file (nts = %d) does not equal number of time steps (NT = %d)!",nts,NT);
-			err(errormessage);
-		}
-	}
-	
-	signals=fmatrix(1,nsrc,1,NT);
-	
-	for (nt=1;nt<=NT;nt++){
-			t=(float)nt*DT;
-			
-			for (k=1;k<=nsrc;k++) {
-				tshift=srcpos_loc[4][k];
-				fc=srcpos_loc[5][k];
-				a=srcpos_loc[6][k];
-				ts=1.0/fc;
+            switch (SOURCE_SHAPE)
+            {
+                case 1: /* Ricker derivative (like SOFI)*/
+                    tau = PI * (t - 1.5 * ts - tshift) / (ts);
+                    amp = (((1.0 - 2.0 * tau * tau) * exp(-tau * tau)));
+                    /**/
+                    break;
+                case 2: /* fumue */
+                    if ((t < tshift) || (t > (tshift + ts)))
+                        amp = 0.0;
+                    else
+                        amp = ((sin(2.0 * PI * (t - tshift) * fc) - 0.5 * sin(4.0 * PI * (t - tshift) * fc)));
 
-				switch (SOURCE_SHAPE){
-					case 1 : /* Ricker */
-						tau=PI*(t-1.5*ts-tshift)/(ts);
-						amp=(((1.0-2.0*tau*tau)*exp(-tau*tau)));
-					break;
-					case 2 : /* fumue */
-						if ((t<tshift) || (t>(tshift+ts))) amp=0.0;
-						else amp=((sin(2.0*PI*(t-tshift)*fc) 
-			    				-0.5*sin(4.0*PI*(t-tshift)*fc)));
-
-/*						amp=((sin(2.0*PI*(t+tshift)*fc) 
+                    /*						amp=((sin(2.0*PI*(t+tshift)*fc) 
 			    				-0.5*sin(4.0*PI*(t+tshift)*fc)));
 */
-					break;
-					case 3 :/* source wavelet from file SOURCE_FILE */
-						amp=psource[nt];
-					break;
-					case 4 : /* sinus raised to the power of three */
-						if ((t<tshift) || (t>(tshift+ts))) amp=0.0;
-						else amp=(0.75*PI/ts)*(pow(sin(PI*(t-tshift)/ts),3.0));
-						break;
-					default : 
-						err("Which source-wavelet ? ");
-						break;
-					}
-					
-					
-					signals[k][nt]=amp*a;
-		}
-	}
-	
-	fprintf(FP," Message from function wavelet written by PE %d \n",MYID);
-	fprintf(FP," %d source positions located in subdomain of PE %d \n",nsrc,MYID);
-	fprintf(FP," have been assigned with a source signal. \n");
-			
-		
-	if (SOURCE_SHAPE==3) free_vector(psource,1,NT);
-	
-	return signals;	
+                    break;
+                case 3: /* source wavelet from file SOURCE_FILE */
+                    amp = psource[nt];
+                    break;
+                case 4: /* sinus raised to the power of three */
+                    if ((t < tshift) || (t > (tshift + ts)))
+                        amp = 0.0;
+                    else
+                        amp = (0.75 * PI / ts) * (pow(sin(PI * (t - tshift) / ts), 3.0));
+                    break;
+                case 5: /* Ricker (like SAVA) */
+                    tau = PI * (t - 1.5 * ts - tshift) / (ts);
+                    amp = (((1.0 - 2.0 * tau * tau) * exp(-tau * tau)));
+                    break;
+                default:
+                    err("Which source-wavelet ? ");
+                    break;
+            }
 
+            signals[k][nt] = amp * a;
+        }
+    }
+
+    // central numeric derivative of the Ricker wavelet to match SOFI3D.
+
+    if (SOURCE_SHAPE == 1)
+    {
+        for (k = 1; k <= nsrc; k++)
+        {
+            for (nt = 1; nt <= NT; nt++)
+            {
+                if (nt == 1)
+                {
+                    amp = signals[k][nt + 1] / 2.0;
+                }
+                if ((nt > 1) && (nt < NT))
+                {
+                    amp_1 = amp;
+                    amp = (signals[k][nt + 1] - signals[k][nt - 1]) / 2.0;
+                    signals[k][nt - 1] = amp;
+                }
+                if (nt == NT)
+                {
+                    amp_1 = amp;
+                    amp = -signals[k][nt - 1] / (2.0 * DT);
+                    signals[k][nt - 1] = amp_1;
+                }
+            }
+            signals[k][NT] = amp;
+        }
+    }
+
+    fprintf(FP, " Message from function wavelet written by PE %d \n", MYID);
+    fprintf(FP, " %d source positions located in subdomain of PE %d \n", nsrc, MYID);
+    fprintf(FP, " have been assigned with a source signal. \n");
+
+    if (SOURCE_SHAPE == 3)
+        free_vector(psource, 1, NT);
+
+    return signals;
 }
