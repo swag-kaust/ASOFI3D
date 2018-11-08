8cca60cc4b0bcb88f1f96794eec297f8c88c726c
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-11-08 12:26:36 +0300

    MAINT: Reformat writemod to improve readability

--- src/writemod.c
+++ src/writemod.c
@@ -19,29 +19,25 @@
  * writedsk    To see possible file formats.
  *
  */
+void writemod(char modfile[STRING_SIZE], float ***q, int format) {
+    // External (global) variables.
+    extern int NX, NY, NZ, POS[4], IDX, IDY, IDZ;
 
+    int i, j, k;
+    FILE *fpmod;
+    char file[STRING_SIZE];
 
-void writemod(char modfile[STRING_SIZE], float ***q, int format){
-	// External (global) variables.
-	extern int NX, NY, NZ, POS[4], IDX, IDY, IDZ;
+    /*printf("\n\n PE %d is writing model to \n",MYID);*/
+    sprintf(file, "%s.%i%i%i", modfile, POS[1], POS[2], POS[3]);
+    /*printf("\t%s\n\n", file);*/
+    fpmod = fopen(file, "w");
+    for (k = 1; k <= NZ; k += IDZ) {
+        for (i = 1; i <= NX; i += IDX) {
+            for (j = 1; j <= NY; j += IDY) {
+                writedsk(fpmod, q[j][i][k], format);
+            }
+        }
+    }
 
-
-	int i, j, k;
-	FILE *fpmod;
-	char file[STRING_SIZE];
-
-	/*printf("\n\n PE %d is writing model to \n",MYID);*/
-	sprintf(file,"%s.%i%i%i",modfile,POS[1],POS[2],POS[3]);
-	/*printf("\t%s\n\n", file);*/
-	fpmod=fopen(file,"w");
-	for (k=1;k<=NZ;k+=IDZ)
-	for (i=1;i<=NX;i+=IDX)
-	for (j=1;j<=NY;j+=IDY)
-		writedsk(fpmod, q[j][i][k],format);
-				
-	fclose(fpmod);
-
-
+    fclose(fpmod);
 }
-
-
