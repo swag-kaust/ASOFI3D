69dafd66ee145e133e4f205ee2db841e17b753f5
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-12-11 13:28:12 +0300

    MAINT: Rename variable aaaaa to config for readability

--- mfiles/read_asofi3D_json.m
+++ mfiles/read_asofi3D_json.m
@@ -1,9 +1,9 @@
-function aaaaa = read_asofi3D_json(filename)
+function config = read_asofi3D_json(filename)
 
 json_text = fileread(filename);
 i = find(json_text=='{');
 j = find(json_text=='}');
 b = json_text(i:j);
-aaaaa = jsondecode(b);
+config = jsondecode(b);
 
 end
