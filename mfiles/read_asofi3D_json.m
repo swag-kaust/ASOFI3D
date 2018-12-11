427588990585402ede4508e8f2ace1a9637143f2
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-12-11 14:31:20 +0300

    MAINT: Add comment and rename output var in read_asofi3D_json

--- mfiles/read_asofi3D_json.m
+++ mfiles/read_asofi3D_json.m
@@ -1,9 +1,12 @@
-function aaaaa = read_asofi3D_json(filename)
+function config = read_asofi3D_json(filename)
+%READ_ASOFI3D_JSON  Read configuration file into `struct`.
+%   json_config = read_asofi3D_json('in_and_out/sofi3D.json') reads file
+%   'in_and_out/sofi3D.json' relative to the current directory.
 
 json_text = fileread(filename);
 i = find(json_text=='{');
 j = find(json_text=='}');
 b = json_text(i:j);
-aaaaa = jsondecode(b);
+config = jsondecode(b);
 
 end
