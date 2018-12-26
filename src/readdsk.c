e3f10c3ebb029fa9d85c79489ced6a4bd486a252
Dmitry Kabanov <kabanovdmitry@gmail.com>
2018-12-27 00:11:57 +0300

    STY: Format readdsk.c with `clang-format`

--- src/readdsk.c
+++ src/readdsk.c
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------
- *   Read one single amplitude from file                                   
+ *   Read one single amplitude from file
  *
  *  ----------------------------------------------------------------------*/
 
@@ -12,36 +12,34 @@ format=2  :  ASCII
 format=3  :  BINARY (IEEE)
 */
 
-
-float readdsk(FILE *fp_in, int format){
-	float amp = 0.0;
+float readdsk(FILE *fp_in, int format)
+{
+    float amp = 0.0;
 
     size_t nelems;
 
-
-	switch(format){
-                case 1 : /* SU*/ 
-                        err(" Sorry, SU-format for snapshots not implemented yet. \n");
-                        break;
-		case 2 :  /*ASCII*/
-                        if (fscanf(fp_in,"%e\n", &amp) != 1) {
-                            err("[readdsk] Could not read an amplitude "
-                                "from a file in ASCII format\n");
-                        } 
-                        break;
-		case 3 :   /* BINARY */
-
-			nelems = fread(&amp, sizeof(float), 1, fp_in);
-            if (nelems != 1) {
-                            err("[readdsk] Could not read an amplitude "
-                                "from a file in binary format\n");
+    switch (format) {
+        case 1: /* SU*/
+            err(" Sorry, SU-format for snapshots not implemented yet. \n");
+            break;
+        case 2: /*ASCII*/
+            if (fscanf(fp_in, "%e\n", &amp) != 1) {
+                err("[readdsk] Could not read an amplitude "
+                    "from a file in ASCII format\n");
             }
-              		break;
-	                
-		default :
-			printf(" Don't know the format for the snapshot-data !\n");
-			err(" No output was written. ");
-	}
+            break;
+        case 3: /* BINARY */
+            nelems = fread(&amp, sizeof(float), 1, fp_in);
+            if (nelems != 1) {
+                err("[readdsk] Could not read an amplitude "
+                    "from a file in binary format\n");
+            }
+            break;
 
-	return amp;
+        default:
+            printf(" Don't know the format for the snapshot-data !\n");
+            err(" No output was written. ");
+    }
+
+    return amp;
 }
